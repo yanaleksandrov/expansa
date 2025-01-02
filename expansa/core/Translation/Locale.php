be3870278eb4d4984571a76b594f291739150f2b
yanal <yanalexandrov@gmail.com>
2025-01-02 14:14:08 +0300

    Move translation to facade

---
+++ expansa/core/Translation/Locale.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Expansa\Translation;
+
+/**
+ * The Locale class is responsible for retrieving and storing the user's locale
+ * based on the HTTP `Accept-Language` header. It provides a method to fetch the
+ * locale in a standardized format, replacing underscores with hyphens.
+ *
+ * @package Expansa
+ */
+class Locale
+{
+    /**
+     * Stores the locale determined from the HTTP request.
+     *
+     * @var string
+     * @since 2025.1
+     */
+    private static string $locale;
+
+    /**
+     * Get local from HTTP.
+     *
+     * @param string $default
+     * @return string
+     *
+     * @since 2025.1
+     */
+    protected function getLocale(string $default = 'en-US'): string
+    {
+        if (! isset(self::$locale) && function_exists('locale_accept_from_http')) {
+            self::$locale = locale_accept_from_http($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? $default);
+        }
+        return str_replace('_', '-', self::$locale ?? $default);
+    }
+}
