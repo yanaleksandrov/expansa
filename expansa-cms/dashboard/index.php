2fa9175941caeb0bb28377cf9782000ed48b3bae
yanal <yanalexandrov@gmail.com>
2025-03-09 23:41:00 +0300

    Add mynaui icons

--- expansa-cms/dashboard/index.php
+++ expansa-cms/dashboard/index.php
@@ -57,7 +57,7 @@ new class
          */
         $suffix = ! Is::debug() ? '.min' : '';
         $styles = [
-            'phosphor', 'air-datepicker', 'colorist', 'drooltip', 'slimselect', 'dialog', 'expansa', 'controls', 'utility', 'notifications', 'nav-editor',
+            'mynaui', 'phosphor', 'air-datepicker', 'colorist', 'drooltip', 'slimselect', 'dialog', 'expansa', 'controls', 'utility', 'notifications', 'nav-editor',
         ];
         foreach ($styles as $style) {
             Asset::style($style, url("/dashboard/assets/css/$style$suffix.css"));
